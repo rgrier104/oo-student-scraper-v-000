3557536958cdf1c675b9d4efcebd15d9c7e0b71a
Rachel Grier <rgrier104@gmail.com>
2018-10-20 19:04:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |key,value|
-      self.send("#{key}="),value)
+      self.send("#{key}=",value)
     end
     self
   end
