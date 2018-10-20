9f35b631320ce95d6bc2c115efcdf55a5598385d
Rachel Grier <rgrier104@gmail.com>
2018-10-20 18:47:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,8 +31,8 @@ class Scraper
         student_prof[:blog] = social.attribute("href").value
       end
     end
-    student[:profile_quote] = doc.css(".profile-quote").text
-    student[:bio] = doc.css(".description-holder").css("p").text
+    student_prof[:profile_quote] = doc.css(".profile-quote").text
+    student_prof[:bio] = doc.css(".description-holder").css("p").text
   end
 
 end
